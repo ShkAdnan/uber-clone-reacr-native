ca9e5a2b2fcc50676b74ae989d2d4eb29a85cfaa
ShkAdnan <adnanasad@outlook.com>
2022-05-30 20:12:08 +0500

    First Commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import tw from 'twrnc';
+import NavOptions from '../components/NavOptions';
+
+const HomeScreen = () => {
+    return (
+      <View style={tw`bg-white h-full`}>
+          <View style={tw`p-5`}>
+              <Image 
+                style={{
+                    width : 100,
+                    height : 100,
+                    resizeMode : 'contain'
+                }}
+                source={{
+                    uri : 'https://links.papareact.com/gzs'
+                }}
+              />
+            <NavOptions />
+          </View>
+      </View>
+    )
+}
+ 
+  
+
+export default HomeScreen
+
+const styles =  StyleSheet.create({
+
+});
+
---
+++ screens/MapScreen.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native';
+import tw from 'twrnc';
+
+const Mapcreen = () => {
+    return (
+      <View style={tw`bg-white h-full`}>
+         
+      </View>
+    )
+}
+ 
+  
+
+export default Mapcreen
+
+const styles =  StyleSheet.create({
+
+});
+
