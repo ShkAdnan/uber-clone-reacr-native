caa356c3a9fa8f750bec2d462eeec1883bd5e1ef
ShkAdnan <adnanasad@outlook.com>
2022-05-30 20:51:43 +0500

    Navigater added

--- App.js
+++ App.js
@@ -4,13 +4,37 @@ import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 import { SafeAreaView } from 'react-native-safe-area-context'
+import "react-native-gesture-handler";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
     <Provider store={ store }>
-      <SafeAreaView>
-        <HomeScreen />
-      </SafeAreaView>
+      <NavigationContainer>    
+        <SafeAreaView>
+          <Stack.Navigator>
+            <Stack.Screen 
+              name='HomeScreen'
+              component={ HomeScreen }
+              options={{
+                headerShown : false
+              }}
+            />
+            <Stack.Screen 
+              name='MapScreen'
+              component={ MapScreen }
+              options={{
+                headerShown : false
+              }}
+            />
+          </Stack.Navigator>
+          <HomeScreen />
+        </SafeAreaView>
+      </NavigationContainer>
     </Provider>
   );
 }
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native';
 import tw from 'twrnc';
 
-const Mapcreen = () => {
+const MapScreen = () => {
     return (
       <View style={tw`bg-white h-full`}>
-         
+         <Text>Map Screen</Text>
       </View>
     )
 }
  
   
 
-export default Mapcreen
+export default MapScreen
 
 const styles =  StyleSheet.create({
 
